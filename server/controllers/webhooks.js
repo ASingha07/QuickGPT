cfe0f80e57a532f04c1d626d56f48c9af651afa1
Atin Singha <atinkumarsingha2002@gmail.com>
2025-09-15 08:38:25 +0530

    currect signature

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -4,7 +4,7 @@ import User from "../models/User.js"
 
 export const stripeWebhooks = async (request, response) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    const sig = request.headers["strip-signature"]
+    const sig = request.headers["stripe-signature"]
 
     let event;
 
